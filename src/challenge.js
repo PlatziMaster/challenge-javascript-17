de7e9947562f236fcf841172756e173cd8941b56
Razielini <razielco@gmail.com>
2020-05-06 21:44:02 -0600

    Callenge Solved With Deque
    I dont know, but my github cant push new mods.

--- src/challenge.js
+++ src/challenge.js
@@ -1,10 +1,29 @@
 const { Deque } = require ('./deque.js');
 
 function palindromeChecker(text) {
+  if (!text) {
+    return false
+  }
 
-  // your code
+  const re = /[^A-Za-z0-9]/g;
+  text = text.toLowerCase().replace(re, '')
 
-  return;
+  let Deq = new Deque()
+  for (var i = 0; i < text.length; i++)
+  {
+    Deq.addFront(text.charAt(i))
+  }
+
+  for(var i = 1; i <= Deq.size(); i++) {
+    if (Deq.peekBack() !== Deq.peekFront()) {
+      return false
+    } else {
+      Deq.removeBack()
+      Deq.removeFront()
+    }
+  }
+
+  return true;
 }
 
 module.exports = { palindromeChecker };
---
+++ src/deque.js
@@ -0,0 +1,95 @@
+class Deque {
+
+  constructor() {
+    this.count = 0;
+    this.topCount = 0;
+    this.items = {};
+  }
+
+  addFront(element) {
+    if (this.isEmpty()) {
+      this.addBack(element);
+    } else if (this.topCount > 0) {
+      this.topCount--;
+      this.items[this.topCount] = element;
+    } else {
+      for (let i = this.count; i > 0; i--) {
+        this.items[i] = this.items[i - 1];
+      }
+      this.count++;
+      this.topCount = 0;
+      this.items[0] = element;
+    }
+  }
+
+  addBack(element) {
+    this.items[this.count] = element;
+    this.count++;
+  }
+
+  removeFront() {
+    if (this.isEmpty()) {
+      return undefined;
+    }
+    const rta = this.items[this.topCount];
+    delete this.items[this.topCount];
+    this.topCount++;
+    return rta;
+  }
+
+  removeBack() {
+    if (this.isEmpty()) {
+      return undefined;
+    }
+    this.count--;
+    const result = this.items[this.count];
+    delete this.items[this.count];
+    return result;
+  }
+
+  size() {
+    return this.count - this.topCount;
+  }
+
+  isEmpty() {
+    return this.size() === 0;
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  peekFront() {
+    if (this.isEmpty()) {
+      return undefined;
+    }
+    return this.items[this.topCount];
+  }
+
+  peekBack() {
+    if (this.isEmpty()) {
+      return undefined;
+    }
+    return this.items[this.count - 1];
+  }
+
+  toString() {
+    if (this.isEmpty()) {
+      return '';
+    }
+    let result = '';
+    for (let index = this.topCount; index < this.count; index++) {
+      result+= `${this.items[index]},`;
+    }
+    return result;
+  }
+
+  clear() {
+    this.count = 0;
+    this.topCount = 0;
+    this.items = {};
+  }
+
+}
+
+module.exports = { Deque };
