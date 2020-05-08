eb2d0a763851d54b2d346e806e11cbccc3da75fd
Luis Antonio Rodriguez Garcia <luis_rdz5@hotmail.com>
2020-05-08 14:26:34 -0500

    Solved the palindrome challenge

--- src/challenge.js
+++ src/challenge.js
@@ -1,10 +1,20 @@
 const { Deque } = require ('./deque.js');
 
 function palindromeChecker(text) {
-
-  // your code
-
-  return;
+  const deque = new Deque();
+  if(!text){
+    return false 
+  }
+  text=text.toLowerCase();
+  text.replace(/\s/g, '').split("").map((item) => {
+    deque.addFront(item);
+  })
+  while(deque.size() > 1){
+    if(deque.removeFront() != deque.removeBack() ){
+      return false;
+    }
+  }
+  return true;
 }
 
 module.exports = { palindromeChecker };
