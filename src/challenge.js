b67e2d96940ee6c078cb55bbc36abc9eeddc328a
Razielini <razielco@gmail.com>
2020-05-09 17:40:40 -0600

    Update challenge.js

--- src/challenge.js
+++ src/challenge.js
@@ -1,20 +1,15 @@
 const { Deque } = require ('./deque.js');
 
 function palindromeChecker(text) {
-  if (!text) {
-    return false
-  }
+  if (!text) return false
 
   const re = /[^A-Za-z0-9]/g;
   text = text.toLowerCase().replace(re, '')
 
   let Deq = new Deque()
-  for (var i = 0; i < text.length; i++)
-  {
-    Deq.addFront(text.charAt(i))
-  }
+  for (let i = 0; i < text.length; i++) Deq.addBack(text.charAt(i))
 
-  for(var i = 1; i <= Deq.size(); i++) {
+  for(let i = 1; i <= Deq.size(); i++) {
     if (Deq.peekBack() !== Deq.peekFront()) {
       return false
     } else {
