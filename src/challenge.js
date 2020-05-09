a9a5123d48bcbe82f2241fe1d6903645c801ccc4
Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>
2020-05-09 17:46:35 -0500

    [Add] I change the constructor of Deque, now it accept a iterable. Challege solved

--- src/challenge.js
+++ src/challenge.js
@@ -1,10 +1,26 @@
-const { Deque } = require ('./deque.js');
+const { Deque } = require('./deque.js');
 
+/**
+ * 
+ * @param {string} text 
+ * @returns {boolean}
+ */
 function palindromeChecker(text) {
+  if (!text) return false;
+  
+  const dequeText = new Deque(
+    text.replace(/ /g, '').toLowerCase()
+  );
 
-  // your code
+  while (!dequeText.isEmpty()) {
+    if (dequeText.peekBack() !== dequeText.peekFront()) {
+      return false;
+    }
+    dequeText.removeBack();
+    dequeText.removeFront();
+  }
 
-  return;
+  return true;
 }
 
 module.exports = { palindromeChecker };
