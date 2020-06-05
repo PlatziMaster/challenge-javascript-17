53af6bb8cadae12e12b401932a52fd27a4d89c5a
Jedp <jedp082@gmail.com>
2020-06-05 16:22:36 -0500

    Challenge done

--- src/challenge.js
+++ src/challenge.js
@@ -2,9 +2,35 @@ const { Deque } = require ('./deque.js');
 
 function palindromeChecker(text) {
 
-  // your code
+  if(!text || text === ''){
+    return false;
+  }
 
-  return;
+  let deque = new Deque();
+  deque.topCount = text.length;
+
+  let lowerText = text.toLowerCase();
+  let toRigthText;
+  let toLeftText = {};
+
+  for(let i = 0; i < lowerText.length; i++){
+    if(text[i] != ' '){
+      deque.addBack(lowerText[i]);
+    }
+  }
+  deque.topCount = deque.count-1;
+  toRigthText = deque.getItems();  
+
+  for(let i = 0; i <= deque.topCount; i++){
+    toLeftText[i] = toRigthText[deque.topCount-i];
+  }
+
+  for(let i = 0; i <= deque.topCount; i++){
+    if(toRigthText[i] !== toLeftText[i]){
+      return false;
+    }
+  }
+  return true;
 }
 
 module.exports = { palindromeChecker };
