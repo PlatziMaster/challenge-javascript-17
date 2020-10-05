c8721bf8acac49fc8d88a608b715d9d27a2331ec
cargdev <ingecarlos.gutierrez@gmail.com>
2020-10-05 02:46:39 -0500

    Adding functions to check palindromes

--- src/challenge.js
+++ src/challenge.js
@@ -1,10 +1,22 @@
-const { Deque } = require ('./deque.js');
+'use strict'
 
-function palindromeChecker(text) {
-
-  // your code
-
-  return;
+const checker = (text) => () => {
+  const textJoin = text.split(' ').join('')
+  const reverseText = textJoin.split('').reverse().join('')
+  if (textJoin === reverseText) {
+    return true
+  }
+  return false
 }
 
+function palindromeChecker(text) {
+  if (text === null || text === undefined || text === '') {
+    return false
+  }
+  const result = checker(text.toLowerCase())
+  return result()
+}
+
+
+console.log(palindromeChecker('Was it a car or a cat I saw'))
 module.exports = { palindromeChecker };
